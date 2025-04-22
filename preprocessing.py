689348f3a75b41bb19a1f0699347ed6a58397ffa
Aaro Junnikkala <aaro.junnikkala@hotmail.com>
2025-04-22 17:41:40 +0300

    add function for initial cleaning and sorting of csv files

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pickle
 from pathlib import Path
 from collections import defaultdict
+from tqdm import tqdm
 
 def get_csv_file_paths():
     """Returns full paths to all CSV files in the ../root/raw_csvs directory.
@@ -20,6 +21,40 @@ def get_csv_file_paths():
     raw_csvs_dir = current_path.parent.parent / "raw_csvs"
     return list(raw_csvs_dir.glob("*.csv"))
 
+def clean_csv_file(raw_csv_path, chunk_size=100000):
+    """Removes unnecessary columns (floor_name, zone_name) and decimals.
+       Writes new csv files to clean_csvs/."""
+    
+    current_path = Path(__file__).resolve()
+    output_dir = current_path.parent.parent / "clean_csvs"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    output_name = raw_csv_path.stem + "_clean.csv"
+    output_path = output_dir / output_name
+
+    use_columns = ["time", "agent_id", "x_axis", "z_axis"]
+    float_columns = ["x_axis", "z_axis"]
+
+    cleaned_chunks = []
+
+    print("Cleaning file (Removing extra columns and decimals)")
+    for chunk in pd.read_csv(raw_csv_path, usecols=use_columns, chunksize=chunk_size):
+        chunk[float_columns] = chunk[float_columns].round(3) # Round float columns to 3 decimals
+        cleaned_chunks.append(chunk)
+
+    # Combine cleaned chunks
+    print("Concatenating chunks...")
+    full_df = pd.concat(cleaned_chunks, ignore_index=True)
+
+    # Sort all data by agent_id and time
+    print("Sorting full DataFrame by agent_id and time...")
+    full_df.sort_values(by=["agent_id", "time"], inplace=True)
+
+    # Save final output
+    print("Writing to CSV...")
+    full_df.to_csv(output_path, index=False)
+
+    print(f"Cleaned file saved to: {output_path}")
 
 
 if __name__ == "__main__":
@@ -27,3 +62,4 @@ if __name__ == "__main__":
     print(f"Found {len(raw_csv_paths)} CSV files:")
     for path in raw_csv_paths:
         print(" -", path)
+    clean_csv_file(raw_csv_paths[1])
