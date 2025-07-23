9612006087fc595d8ac1f0d6d98771b5d15ea7b2
Aaro Junnikkala <aaro.junnikkala@hotmail.com>
2025-07-23 09:34:33 +0300

    initial commit

---
+++ obstacle_zones.py
@@ -0,0 +1,8 @@
+def obstacle_zones():
+    # Define obstacle zones as (x_min, x_max, y_min, y_max)
+    zones = [
+        (132.0, 137.5, -170.5, -164.5),   # Lower pillar
+        (132.0, 137.5, -155.0, -149.0)    # Upper pillar
+    ]
+
+    return zones
